482d32ad0c02f3aa29ca46388f6af0f5476263b6
macbeth <layne.wegenast@gmail.com>
2022-05-13 22:32:58 -0500

    started applying tailwind to homepage

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        bluePurple: '#5C5AC9',
+        white: '#FFFFFF',
+        darkerBlue: '#2C3487',
+        brightRed: '#FF4400',
+        medBlue: '#40A4DD',
+        black:'#000000'
+      }
+    },
+    fontFamily: {
+      'sans': ['Asap', 'sans-serif'],
+  },
+  plugins: [],
+}} 
