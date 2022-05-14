ea48e5d0f18b34caabb8ed63e718be129ef265d5
macbeth <layne.wegenast@gmail.com>
2022-05-14 15:05:34 -0500

    working on login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,14 @@ module.exports = {
         darkerBlue: '#2C3487',
         brightRed: '#FF4400',
         medBlue: '#40A4DD',
-        black:'#000000'
-      }
+        black:'#000000',
+        lightPurp: '#EBEBF9',
+        darkGray: '#65656B'
+      },
+      padding: {
+        '1/2': '50%',
+        full:'100%',
+      },
     },
     fontFamily: {
       'sans': ['Asap', 'sans-serif'],
