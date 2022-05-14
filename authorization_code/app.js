f0ecc101938232fa53ee2f658d9a8f90065b0133
MJB <matt.berti13@gmail.com>
2022-05-14 10:30:40 -0500

    added OAuth on Backend

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -47,7 +47,8 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-library-read';
+  var scope = 'user-read-private user-read-email';
+
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS crescendo_db;
+
+CREATE DATABASE crescendo_db;
+
