aea25d1918b9d8249f441385fee76f6a3c266e9b
Xan <tsadiktalmudim@gmail.com>
2022-05-17 14:43:55 -0500

    added seeds folder and files

--- models/Comments.js
+++ models/Comments.js
@@ -1,45 +1,51 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Comment extends Model { }
+class Comment extends Model {}
 
 Comment.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        comment_text: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        song_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'song',
-                key: 'id'
-            }
-        }
-        //playlist_id
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
-    }
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    song_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "song",
+        key: "id",
+      },
+    },
+    playlist_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "playlist",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
 );
 
 module.exports = Comment;
