5370de76121b40794c086383c1e29c2e6adf8c55
obregonscott <obregon.scott@gmail.com>
2022-05-17 14:32:26 -0500

    added Homeepage JS for nav open/close

---
+++ public/javascript/homepage.js
@@ -0,0 +1,10 @@
+var button = document.querySelector('#button');
+var menu = document.querySelector('#menuBig');
+
+button.addEventListener('click', () => {
+  if (menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden');
+  } else {
+      menu.classList.add('hidden');
+  }
+})
