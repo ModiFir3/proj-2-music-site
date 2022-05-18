b6c644f59eb18dbb7001d40cd88d38cdb18c476e
MJB <matt.berti13@gmail.com>
2022-05-17 19:41:24 -0500

    merge conflict in server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-    app.use(cors());
-    app.use(cookieParser());
+    // app.use(cors());
+    // app.use(cookieParser());
 
 
 //turn on routes
