3678803b9358da6272e3b0ca68da95f2408c6e37
MJB <matt.berti13@gmail.com>
2022-05-17 19:45:49 -0500

    merge paul and develop

--- server.js
+++ server.js
@@ -11,25 +11,21 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers })
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-=======
->>>>>>> 3d18934b5084d084bb6a462a5ec38abff7a4c641
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
-<<<<<<< HEAD
-=======
-=======
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
->>>>>>> 72f9f9224bc72835aef4432c9a1a35f8277178f8
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -39,10 +35,7 @@ app.use(express.static('public'));
 
 
 //turn on routes
-<<<<<<< HEAD
->>>>>>> 3d18934b5084d084bb6a462a5ec38abff7a4c641
-=======
->>>>>>> 72f9f9224bc72835aef4432c9a1a35f8277178f8
+
 app.use(routes);
 
 
