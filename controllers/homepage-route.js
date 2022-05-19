bfbbe9f1fc4b6b64b812b8b6eb92a94eefea8125
PaulS <psoliz333@gmail.com>
2022-05-18 19:25:22 -0500

    created examples for backend logic on playlist

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Comment, Playlist, Song, User } = require('../models')
 
 //TODO: Add Session to home routes for users to see different data if loggedIn(matt)
 
@@ -9,6 +10,30 @@ router.get('/', (req, res) => {
 
 router.get('/login', (req, res) => {
     res.render('login')
+})
+
+router.get('/playlists', (req, res) => {
+    Playlist.findAll({
+        attributes: [
+            'id',
+            'playlist_name',
+            'author',
+            'embed_playlist',
+        ],
+    })
+        .then(dbPlaylistData => {
+            // res.render('playlist', dbPlaylistData[0].get({ plain: true }));
+
+            const playlists = dbPlaylistData.map(playlist => playlist.get({ plain: true }));
+
+            res.render('playlist', {
+                playlists
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 })
 
 module.exports = router;
