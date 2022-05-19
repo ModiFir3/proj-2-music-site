1c7c971ad8ce13b8d77446b9fa26853be624dfcc
MJB <matt.berti13@gmail.com>
2022-05-18 19:18:29 -0500

    added initial logout logic

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
-//TODO: Add Session to home routes for users to see different data if loggedIn(matt)
+
 
 router.get('/', (req, res) => {
-    res.render('homepage')
-})
+    // if(req.session){
+    //     //TODO: Add Session to home routes for users to see different data if loggedIn(matt)
+    //     res.render('#')
+    // }else
+    // {
+        res.render('homepage')
+    // }
+});
 
 router.get('/login', (req, res) => {
     res.render('login')
