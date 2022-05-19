77062fde5ac7b85e69797bd96f50b2a25029280b
PaulS <psoliz333@gmail.com>
2022-05-18 20:37:34 -0500

    single playlist

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -34,6 +34,32 @@ router.get('/playlists', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         })
+});
+
+router.get('/playlists/:id', (req, res) => {
+    Playlist.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'playlist_name',
+            'author',
+            'embed_playlist',
+        ]
+    })
+        .then(dbPlaylistData => {
+
+            const playlists = dbPlaylistData.map(playlist => playlist.get({ plain: true }));
+
+            res.render('single-playlist', {
+                playlists
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 })
 
 module.exports = router;
